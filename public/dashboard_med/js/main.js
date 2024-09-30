e4abd5676f24db961b43614b24ee1dd1bf1bc59e
Rafael <202220909@urp.edu.pe>
2024-09-30 07:42:50 -0400

    calendario dashboard medico

--- public/dashboard_med/js/main.js
+++ public/dashboard_med/js/main.js
@@ -212,3 +212,117 @@ function inicializarApp() {
 
 // Ejecutar funciones al cargar la página
 window.onload = inicializarApp;
+
+const daysContainer = document.querySelector(".days"),
+nextBtn = document.querySelector(".next-btn"),
+prevBtn = document.querySelector(".prev-btn"),
+month = document.querySelector(".month"),
+todayBtn= document.querySelector(".today-btn");
+
+
+const months = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Setiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+];
+
+const days = ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"];
+
+//get current date
+const date = new Date();
+//get current month
+let currentMonth = date.getMonth();
+//get current year
+let currentYear = date.getFullYear();
+
+//function to render days
+function renderCalendar(){
+
+
+    date.setDate(1);
+    const firstDay = new Date(currentYear, currentMonth, 1);
+    const lastDay = new Date(currentYear, currentMonth + 1, 0);
+    const lastDayIndex = lastDay.getDay();
+    const lastDayDate = lastDay.getDate();
+    const prevLastDay = new Date(currentYear, currentMonth, 0);
+    const prevLastDayDate = prevLastDay.getDate();
+    const nextDays = 7 - lastDayIndex - 1;
+
+    month.innerHTML = `${months[currentMonth]} ${currentYear}`;
+    let days = "";
+
+    for(let x= firstDay.getDay(); x > 0; x--){
+        days += `<div class="day prev">${prevLastDayDate -x + 1}</div>`
+    }
+
+
+    for (let i = 1; i<=lastDayDate; i++){
+        if(
+            i=== new Date().getDate() &&
+            currentMonth == new Date().getMonth() &&
+            currentYear == new Date().getFullYear()
+        ){
+            days += `<div class="day today">${i}</div>`
+        }else{
+            days += `<div class="day">${i}</div>`
+        }
+    }
+
+    for (let j = 1; j<=nextDays; j++){
+        days += `<div class="day next">${j}</div>`;
+    }
+
+
+    hideTodayBtn();
+    daysContainer.innerHTML=days;
+
+
+
+}
+
+renderCalendar();
+
+nextBtn.addEventListener("click", ()=>{
+    currentMonth++;
+    if(currentMonth > 11){
+       currentMonth = 0;
+       currentYear++;
+    }
+    renderCalendar();
+})
+
+prevBtn.addEventListener("click", ()=>{
+    currentMonth--;
+    if(currentMonth < 0){
+        currentMonth = 11;
+        currentYear--;
+    }
+    renderCalendar();
+});
+
+todayBtn.addEventListener("click",() =>{
+    currentMonth = date.getMonth();
+    currentYear=date.getFullYear();
+
+    renderCalendar();
+})
+
+function hideTodayBtn(){
+    if(
+        currentMonth === new Date ().getMonth()&&
+        currentYear === new Date().getFullYear()
+    ){
+        todayBtn.style.display = "none";
+    } else{
+        todayBtn.style.display = "flex";
+    }
+}
